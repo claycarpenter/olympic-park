aafe1acdf04485e2436c18fa8d68195cd797aaed
Clay Carpenter <claycarpenter@gmail.com>
2015-07-06 14:50:13 -0400

    Prevents default behavior on hamburger icon click.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   // Toggle dropdown nav visibility on click.
-  $('.hamburger').on('click', function () {
+  $('.hamburger').on('click', function (event) {
+    event.preventDefault();
     $('header ul').toggle();
   });
 });
