ed35527816f2cbce5e930744a43799a342747dc3
Clay Carpenter <claycarpenter@gmail.com>
2015-07-07 23:40:52 -0400

    Updates nav functionality.

--- js/app.js
+++ js/app.js
@@ -4,4 +4,8 @@ $(document).ready(function () {
     event.preventDefault();
     $('.nav-header').toggleClass('open-menu');
   });
+
+  $('.nav-options a').on('click', function (event) {
+    $('.nav-header').removeClass('open-menu');
+  });
 });
